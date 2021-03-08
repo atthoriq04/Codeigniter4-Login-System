3d175e862bf2773e6670ab4ab900a2889b9119e6
thoriqy1 <46284371+thoriqy1@users.noreply.github.com>
2021-03-08 18:54:43 +0700

    Add files via upload

---
+++ system.sql
@@ -0,0 +1,165 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Mar 08, 2021 at 11:48 AM
+-- Server version: 5.7.24
+-- PHP Version: 7.4.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `system`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `module`
+--
+
+CREATE TABLE `module` (
+  `Moduleid` int(11) NOT NULL,
+  `Modulename` varchar(25) NOT NULL,
+  `Moduleurl` varchar(260) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `module`
+--
+
+INSERT INTO `module` (`Moduleid`, `Modulename`, `Moduleurl`) VALUES
+(1, 'Dashboard', '/'),
+(2, 'Menu', '/Home/Menu');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role`
+--
+
+CREATE TABLE `role` (
+  `Roleid` int(11) NOT NULL,
+  `Rolename` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `role`
+--
+
+INSERT INTO `role` (`Roleid`, `Rolename`) VALUES
+(1, 'Admin'),
+(2, 'User');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role_module`
+--
+
+CREATE TABLE `role_module` (
+  `id` int(11) NOT NULL,
+  `Roleid` int(11) NOT NULL,
+  `Moduleid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `role_module`
+--
+
+INSERT INTO `role_module` (`id`, `Roleid`, `Moduleid`) VALUES
+(1, 1, 1),
+(2, 1, 2),
+(3, 2, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `Userid` int(11) NOT NULL,
+  `Username` varchar(255) NOT NULL,
+  `Userpassword` varchar(255) NOT NULL,
+  `Roleid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`Userid`, `Username`, `Userpassword`, `Roleid`) VALUES
+(2, 'user', '$2y$10$6kSRGi4Y8qu5saOWrV6eh.v4GJNeX35iiZ.3fCUgjGF76lpjAXDs6', 2),
+(3, 'Admin', '$2y$10$6kSRGi4Y8qu5saOWrV6eh.v4GJNeX35iiZ.3fCUgjGF76lpjAXDs6', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `module`
+--
+ALTER TABLE `module`
+  ADD PRIMARY KEY (`Moduleid`);
+
+--
+-- Indexes for table `role`
+--
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`Roleid`);
+
+--
+-- Indexes for table `role_module`
+--
+ALTER TABLE `role_module`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`Userid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `module`
+--
+ALTER TABLE `module`
+  MODIFY `Moduleid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `role`
+--
+ALTER TABLE `role`
+  MODIFY `Roleid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `role_module`
+--
+ALTER TABLE `role_module`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `Userid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
